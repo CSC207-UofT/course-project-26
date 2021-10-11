b997c9c47614efce43bc6584d71c880d904a7732
willwu <daihao.wu@mail.utoronto.ca>
2021-10-11 01:14:09 -0400

    books

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,20 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 public class Book {
-    private boolean isSold;
-    private double price;
-    private String name;
-    private String author;
-    private String publisher;
-    private String pub_date;
-    private int edition;
-    private int pages;
-    private String format;
-    private String language;
-    private double weight;
-    private String size;
 
     /**
      * Construct a Book with the following instance attributes:
@@ -24,7 +13,7 @@ public class Book {
      * @param name      The name of the Book
      * @param author    The author of the Book
      * @param publisher The publisher of the Book
-     * @param pub_date  The publish date of the Book in ... TODO: Choose what format to use for dates
+     * @param pub_date  The publish date of the Book in ...  with the format yyyy-MM-dd in string
      * @param edition   The edition of the Book
      * @param pages     The number of pages in the Book
      * @param format    The format of the Book. It can be either one of "Paperback", "Hardcover", "Looseleaf",
@@ -33,6 +22,19 @@ public class Book {
      * @param weight    The weight of the Book in kilograms. Default value is N/A
      * @param size      The dimensions of the Book. Default value is N/A. TODO: Decide how to store dimensions
      */
+
+    private boolean isSold;
+    private double price;
+    private String name;
+    private String author;
+    private String publisher;
+    private DateTimeFormatter pub_date;
+    private int edition;
+    private int pages;
+    private String format;
+    private String language;
+    private double weight;
+    private String size;
 
     public Book(double price,
                  String name,
@@ -51,7 +53,7 @@ public class Book {
         this.name = name;
         this.author = author;
         this.publisher = publisher;
-        this.pub_date = pub_date;
+        this.pub_date = DateTimeFormatter.ofPattern(pub_date);
         this.edition = edition;
         this.pages = pages;
         this.format = format;
@@ -60,11 +62,8 @@ public class Book {
         this.size = size;
 
     }
-    // TODO: Use overloading to create another constructor with fewer parameters (incase some of the info isn't known about the book when it's initialized)
+    // TODO: Use overloading to create another constructor with fewer parameters (in case some of the info isn't known about the book when it's initialized)
 
-    public boolean isSold() {
-        return this.isSold;
-    }
 
     /**
      * Get the Book info in a list.
@@ -85,12 +84,34 @@ public class Book {
             this.weight,
             this.size);
     }
+    //getter methods start here...
 
-    public void setSold() {
-        this.isSold = true;
-    }
+    public boolean isSold() {return this.isSold;}
+    public String getName(){return this.name;}
+    public String getAuthor(){return this.author;}
+    public String getPublisher(){return this.publisher;}
+    public String getPub_date(){return this.pub_date.toString();}
+    public String getFormat(){return this.format;}
+    public String getLanguage(){return this.language;}
+    public String getSize(){return this.size;}
+    public int getEdition(){return this.edition;}
+    public int getPages(){return this.pages;}
+    public double getPrice(){return this.price;}
+    public double getWeight(){return this.weight;}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    // setter methods start here...
+
+    public void set_isSold() {this.isSold = true;}
+    public void setPrice(double price) {this.price = price;}
+    public void setName(String name){this.name = name;}
+    public void setAuthor(String author){this.author = author;}
+    public void setPublisher(String publisher){this.publisher = publisher;}
+    public void setPub_date(String pub_date){this.pub_date = DateTimeFormatter.ofPattern(pub_date);}
+    public void setEdition(int edition){this.edition = edition;}
+    public void setPages(int pages){this.pages = pages;}
+    public void setFormat(String format){this.format = format;}
+    public void setLanguage(String language){this.language = language;}
+    public void setWeight(double weight){this.weight = weight;}
+    public void setSize(String size){this.size = size;}
+
 }
