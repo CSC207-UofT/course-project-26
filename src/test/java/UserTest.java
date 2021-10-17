c9daeea97e2111590922e844ae7dbe9dae943ed2
basic <yunqi.du@mail.utoronto.ca>
2021-10-16 22:29:24 -0400

    User, seller, buyer half completed

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,8 +1,31 @@
 import Users.User;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class UserTest {
+    private User user;
+
+    @Before
+    public void setUp(){
+        user = new User("annie", "duannie", "yunqi.du@mail.utoronto.ca","123456","Bay");
+    }
+
+    @Test
+    public void testgetName(){
+        assertEquals("annie", user.getName());
+    }
+
+    @Test
+    public void testgetemail(){
+        assertEquals("annie", user.getEmail());
+    }
+
+    @Test
+    public void testgetlocation(){
+        assertEquals("annie", user.getLocation());
+    }
+
 }
